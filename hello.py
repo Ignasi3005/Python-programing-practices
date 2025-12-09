f8d7a4654313dc995361e47be02a6b13a5aa2126
Ignasi3005 <ignasimiho@gmail.com>
2025-12-09 16:22:25 +0300

    added new overlap

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 lab=["Ignasi","Kalisio","Miho"]
 Age=[24,52,86]
+scores=[90,60,50]
 plt.pie(Age,labels=lab)
 plt.title("MIHO CLASS")
+plt.pie(scores,labels=lab)
+plt.title("MIHO EX")
+plt.legend()
 plt.show()
